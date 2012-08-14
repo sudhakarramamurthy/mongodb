99226a09c63ef54bc5444a7c9d8e29d160c6d55d
Sudhakar <sudhakarramamurthy@gmail.com>
2012-08-14 22:04:42 +0530

    First commit

---
+++ src/main/java/su/mvnlab1/PeopleData.java
@@ -0,0 +1,35 @@
+package su.mvnlab1;
+
+public class PeopleData {
+	public String name;
+	public int age;
+	public String type;
+
+
+	public PeopleData() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
